3702847e8abd1310e9bad0b246ac6008cb7a3b0f
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-07-13 19:02:35 +0700

    tinggal finishing

---
+++ rentenir.js
@@ -0,0 +1,48 @@
+const dataPeminjam = [
+    {
+        'nama' : 'Obi',
+        'usia' : 18,
+        'pekerjaan' : 'tidak tetap',
+        'gajiTerakhir' : 2000000
+    },
+    {
+        'nama' : 'Wawan',
+        'usia' : 28,
+        'pekerjaan' : 'tetap',
+        'gajiTerakhir' : 800000
+    },
+    {
+        'nama' : 'Bejo',
+        'usia' : 9,
+        'pekerjaan' : ' tidak tetap',
+        'gajiTerakhir' : 10000
+    },
+    {
+        'nama' : 'Usmar',
+        'usia' : 23,
+        'pekerjaan' : 'tetap',
+        'gajiTerakhir' : 5000000
+    },
+    {
+        'nama' : 'Bowo',
+        'usia' : 10,
+        'pekerjaan' : 'tetap',
+        'gajiTerakhir' : 400000
+    }
+]
+
+let totalBiaya = 0
+let gaji = 0
+
+for (let i = 0 ; i < dataPeminjam.length ; i ++) {
+    const korban = dataPeminjam[i]
+    if (korban.usia >= 18) {
+        if (korban.pekerjaan === 'tetap') {
+            gaji = korban.gajiTerakhir * 2.5
+        }
+        else {
+            gaji = korban.gajiTerakhir * 1.5
+        }
+        console.log(gaji);
+    }    
+}
